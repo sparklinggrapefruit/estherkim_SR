b64d4fed32007caa02f1fe687c6addcf96a0dec2
Esther Kim <76713709+sparklinggrapefruit@users.noreply.github.com>
2024-09-18 01:27:21 -0500

    HW1

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("hello github and the rest of the world. the changes aren't working!")
 print("hello world where is the new pull request?")
 print("why isn't the pull request showing up?")
+print("HW 1 changes")
